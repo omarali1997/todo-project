5f40c76cac3a69d83c6f883b8c4e1fffedea919c
YOUR FULL NAMEomarali1997 <omar.aziz.9718@gmail.com>
2022-08-02 21:24:24 +0300

    add

--- app.js
+++ app.js
@@ -9,8 +9,8 @@
 var userName = prompt("enter your name")
 console.log(userName);
 
-var userAge = prompt("enter your gender(male , female)")
-console.log(userAge);
+var useGender = prompt("enter your gender(male , female)")
+console.log(useGender);
 
 
 var userAge = prompt("enter your age");
@@ -21,3 +21,47 @@ if (userAge <= 0){
 
 var x = confirm("you wants to skip the welcoming message?");
 console.log(x);
+
+if(useGender == "male"){
+    alert("welcome Mr " + userName)
+} else if(useGender == "female")
+{
+    alert("welcome Ms " + userName)
+} else
+{
+    alert("welcome " + userName)
+}
+
+var answers = []
+
+var q1 = prompt("Are you sure of your information")
+if(q1 == "yes"){
+    answers.push("yes");
+}else if(q1 == "no"){
+    answers.push("no");
+}else 
+{
+    answers.push("invalid");
+}
+
+var q2 = prompt("Are you from jordan")
+if(q2 == "yes"){
+    answers.push("yes");
+}else if(q2 == "no"){
+    answers.push("no");
+}else 
+{
+    answers.push("invalid");
+}
+
+var q3 = prompt("Are you happy now")
+if(q3 == "yes"){
+    answers.push("yes");
+}else if(q3 == "no"){
+    answers.push("no");
+}else 
+{
+    answers.push("invalid");
+}
+
+console.log(answers);
